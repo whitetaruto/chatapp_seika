0fa0a861912be9330796c692ecc0e141dcb2eb9c
whitetaruto <89428672+whitetaruto@users.noreply.github.com>
2021-11-06 11:04:15 +0900

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAN5DIcZOT5cEQsZa8QDf6LsEtkXUxdHuY",
-    authDomain: "chat-app-1bc80.firebaseapp.com",
-    projectId: "chat-app-1bc80",
-    storageBucket: "chat-app-1bc80.appspot.com",
-    messagingSenderId: "365586935725",
-    appId: "1:365586935725:web:a446ee02f3016a94017b24"
+    apiKey: "-------------------------",
+    authDomain: "-------------------------",
+    projectId: "-------------------------",
+    storageBucket: "-------------------------",
+    messagingSenderId: "-------------------------",
+    appId: "-------------------------"
 }
 
 firebase.initializeApp(firebaseConfig)
